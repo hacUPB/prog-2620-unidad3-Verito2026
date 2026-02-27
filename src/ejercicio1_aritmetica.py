2dcbedfead6f2207d618369d2c1e51791e21d676
Veronica Alexandra <veronica.micanquer@upb.edu.co>
2026-02-27 14:51:06 -0500

    Realizo cambios en ejercicio1_aritmetica.py

---
+++ src/ejercicio1_aritmetica.py
@@ -0,0 +1,11 @@
+a = 85  
+b = 0.15
+c = 4 
+
+total_propina = a * b 
+total_pagar = total_propina + a 
+pago_individual = total_pagar / c
+
+print("Total a pagar:", total_pagar)
+print("Total de propina:", total_propina) 
+print("Pago individual:", pago_individual)
